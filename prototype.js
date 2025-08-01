a7a7d493a60507cbcd939f700bed267c01ec393f
Aman Kundu <aman.kundu63@gmail.com>
2025-08-01 10:12:31 +0530

    adding a file

---
+++ prototype.js
@@ -0,0 +1,22 @@
+function Person(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+}
+
+Person.prototype.nationality = "English";
+
+
+
+
+function Person2(first, last, age, eyecolor) {
+  this.firstName = first;
+  this.lastName = last;
+  this.age = age;
+  this.eyeColor = eyecolor;
+}
+
+Person2.prototype.name = function() {
+  return this.firstName + " " + this.lastName;
+};
