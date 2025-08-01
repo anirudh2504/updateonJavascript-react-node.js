f530a5d104e469e40ac91cbf07cea4789afe0529
Aman Kundu <aman.kundu63@gmail.com>
2025-08-01 12:03:35 +0530

    add all changes

--- promiss.js
+++ promiss.js
@@ -1,22 +1,46 @@
-const getPromise=()=>
+// const getPromise=()=>
+// {
+//    return new Promise((resolve,reject)=>
+//     {
+//         console.log("i am promise")
+//         resolve("completed successfully");
+//        //reject("Failed");
+
+
+//     });
+// };
+
+
+// let promise=getPromise();
+// promise.then(()=>
+//     {
+//         console.log("Sussess");
+//     })
+//     promise.catch(()=>
+//         {
+//             console.log("Rejected");
+//         })
+
+
+function asyncFunction() 
 {
-   return new Promise((resolve,reject)=>
-    {
-        console.log("i am promise")
-       // resolve("completed successfully");
-       reject("Failed");
-
-
-    });
-};
-
-
-let promise=getPromise();
-promise.then(()=>
-    {
-        console.log("Sussess");
-    })
-    promise.catch(()=>
+    return new Promise((resolve,reject)=>
         {
-            console.log("Rejected");
-        })
+            setTimeout(()=>      //this will fetch data after 5 sec
+                {
+                    console.log("some details");
+                    resolve("Success")
+                },5000);
+           
+        });
+}
+console.log("Fetching data ")
+let p1=asyncFunction();
+p1.then((res)=>
+    {
+        console.log(res);
+    });
+// p1.reject((res)=>
+//     {
+
+//     })    
