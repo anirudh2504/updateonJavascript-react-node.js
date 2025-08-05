129f878e74f2b2a1a1a09c5cf870bb0331bf7b79
Aman Kundu <aman.kundu63@gmail.com>
2025-08-05 12:59:06 +0530

    commit all

--- rock,ppr,secssor/app.js
+++ rock,ppr,secssor/app.js
@@ -11,6 +11,7 @@ const genCompChoice = () => {
   const options = ["rock", "paper", "scissors"];
   const randIdx = Math.floor(Math.random() * 3);
   return options[randIdx];
+  
 };
 
 const drawGame = () => {
@@ -58,6 +59,7 @@ const playGame = (userChoice) => {
 choices.forEach((choice) => {
   choice.addEventListener("click", () => {
     const userChoice = choice.getAttribute("id");
+    playGame(userChoice);
     playGame(userChoice);
   });
 });
