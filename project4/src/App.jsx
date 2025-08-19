3cbc493300dd21a8065c3c7274f0dd3e887767e7
Anirudh2504 <anirajput20022@gmail.com>
2025-08-19 12:11:57 +0530

    commit

--- project4/src/App.jsx
+++ project4/src/App.jsx
@@ -1,11 +1,17 @@
 import Player from './components/Player.jsx';
 import StopWatch from './components/Stopwatch.jsx';
+import TimerChallenges from './components/TimerChallenges.jsx';
 
 function App() {
   return (
     <>
       <Player />
       <div id="challenges">
+        <TimerChallenges title="Easy" targetTime={1}/>
+        <TimerChallenges title="Normal" targetTime={3}/>
+        <TimerChallenges title="Hard" targetTime={10}/>
+        <TimerChallenges title="Very Hard" targetTime={15}/>
+
       </div>
    
      
