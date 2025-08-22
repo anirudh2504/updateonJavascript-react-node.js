6fe19ba225b7d22dc4feb46dc24983b57a195c3f
Anirudh2504 <anirajput20022@gmail.com>
2025-08-22 16:27:33 +0530

    comit

--- forms/src/components/Header.jsx
+++ forms/src/components/Header.jsx
@@ -1,3 +1,15 @@
+// import logoImg from '../assets/logo.jpg';
+
+// export default function Header() {
+//   return (
+//     <header>
+//       <img src={logoImg} alt="A form and a pencil" />
+//       <h1>React Forms</h1>
+//     </header>
+//   );
+// }
+
+
 import logoImg from '../assets/logo.jpg';
 
 export default function Header() {
--- forms/src/components/Signup.jsx
+++ forms/src/components/Signup.jsx
@@ -1,33 +1,146 @@
-import { isEqualsToOtherValue } from "../util/validation";
-export default function Signup() {
+// import { isEqualsToOtherValue } from "../util/validation";
+// export default function Signup() {
 
-    function handleSubmit(event)
-    {
-       event.preventDefault();   
-       const fd=new FormData(event.target);
-       const acd=fd.getAll('acquisition')
-       const data=Object.fromEntries(fd.entries())
-       data.acquisition=acd;
-       console.log(data)
-    }
+//     function handleSubmit(event)
+//     {
+//        event.preventDefault();   
+//        const fd=new FormData(event.target);
+//        const acd=fd.getAll('acquisition')
+//        const data=Object.fromEntries(fd.entries())
+//        data.acquisition=acd;
+//        console.log(data)
+//     }
+//   return (
+//     <form onSubmit={handleSubmit}>
+//       <h2>Welcome on board!</h2>
+//       <p>We just need a little bit of data from you to get you started 🚀</p>
+
+//       <div className="control">
+//         <label htmlFor="email">Email</label>
+//         <input id="email" type="email" name="email" required/>
+//       </div>
+
+//       <div className="control-row">
+//         <div className="control">
+//           <label htmlFor="password">Password</label>
+//           <input id="password" type="password" name="password" required minLength={8}/>
+//         </div>
+
+//         <div className="control">
+//           <label htmlFor="confirm-password"  >Confirm Password</label>
+//           <input
+//             id="confirm-password"
+//             type="password"
+//             name="confirm-password"
+//           />
+//         </div>
+//       </div>
+
+//        <hr /> 
+        
+
+//       <div className="control-row">
+//         <div className="control">
+//           <label htmlFor="first-name">First Name</label>
+//           <input type="text" id="first-name" name="first-name" required />
+//         </div>
+
+//         <div className="control">
+//           <label htmlFor="last-name">Last Name</label>
+//           <input type="text" id="last-name" name="last-name" required />
+//         </div>
+//       </div>
+
+//       <div className="control">
+//         <label htmlFor="phone">What best describes your role?</label>
+//         <select id="role" name="role" required>
+//           <option value="student">Student</option>
+//           <option value="teacher" >Teacher</option>
+//           <option value="employee">Employee</option>
+//           <option value="founder">Founder</option>
+//           <option value="other">Other</option>
+//         </select>
+//       </div>
+//   {/* ----------------------------legend -----------------------------------*/}
+//       <fieldset>
+//         <legend>How did you find us?</legend>
+//         <div className="control">
+//           <input
+//             type="checkbox"
+//             id="google"
+//             name="acquisition"
+//             value="google"
+//           />
+//           <label htmlFor="google">Google</label>
+//         </div>
+
+//         <div className="control">
+//           <input
+//             type="checkbox"
+//             id="friend"
+//             name="acquisition"
+//             value="friend"
+//           />
+//           <label htmlFor="friend">Referred by friend</label>
+//         </div>
+
+//         <div className="control">
+//           <input type="checkbox" id="other" name="acquisition" value="other" />
+//           <label htmlFor="other">Other</label>
+//         </div>
+//       </fieldset>
+
+//       <div className="control">
+//         <label htmlFor="terms-and-conditions">
+//           <input type="checkbox" id="terms-and-conditions" name="terms" required />I
+//           agree to the terms and conditions
+//         </label>
+//       </div>
+
+
+
+
+
+
+
+//       {/*-----------------Buttons------------------- */}
+
+//       <p className="form-actions">
+//         <button type="reset" className="button button-flat">
+//           Reset
+//         </button>
+//         <button type="submit" className="button">
+//           Sign up
+//         </button>
+//       </p>
+//     </form>
+//   );
+// }
+
+
+
+
+
+
+export default function Signup() {
   return (
-    <form onSubmit={handleSubmit}>
+    <form>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" required/>
+        <input id="email" type="email" name="email" />
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" required minLength={8}/>
+          <input id="password" type="password" name="password" />
         </div>
 
         <div className="control">
-          <label htmlFor="confirm-password"  >Confirm Password</label>
+          <label htmlFor="confirm-password">Confirm Password</label>
           <input
             id="confirm-password"
             type="password"
@@ -36,32 +149,31 @@ export default function Signup() {
         </div>
       </div>
 
-       <hr /> 
-        
+      <hr />
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" required />
+          <input type="text" id="first-name" name="first-name" />
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" required />
+          <input type="text" id="last-name" name="last-name" />
         </div>
       </div>
 
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role" required>
+        <select id="role" name="role">
           <option value="student">Student</option>
-          <option value="teacher" >Teacher</option>
+          <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
           <option value="founder">Founder</option>
           <option value="other">Other</option>
         </select>
       </div>
-  {/* ----------------------------legend -----------------------------------*/}
+
       <fieldset>
         <legend>How did you find us?</legend>
         <div className="control">
@@ -92,26 +204,16 @@ export default function Signup() {
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" required />I
+          <input type="checkbox" id="terms-and-conditions" name="terms" />I
           agree to the terms and conditions
         </label>
       </div>
-
-
-
-
-
-
-
-      {/*-----------------Buttons------------------- */}
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
           Reset
         </button>
-        <button type="submit" className="button">
-          Sign up
-        </button>
+        <button className="button">Sign up</button>
       </p>
     </form>
   );
