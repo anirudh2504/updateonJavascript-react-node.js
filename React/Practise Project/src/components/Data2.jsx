c267fafc05118aff5b271a06b6d9d20b8b485c8a
Anirudh2504 <anirajput20022@gmail.com>
2025-08-29 11:01:13 +0530

    commit

--- React/Practise Project/src/components/Data2.jsx
+++ React/Practise Project/src/components/Data2.jsx
@@ -2,45 +2,39 @@ import React, { useEffect, useState } from "react";
 
 function Data2() {
   const [data2, setData2] = useState([]);
-  
-  
- useEffect(() => {
+
+  useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then((res) => res.json())
       .then((data) => setData2(data));
   }, []);
-function handleDelete(index)
-{
-  const updatedData=data2.filter((element,i)=>(index!==i))
-  setData2(updatedData)
- 
+  function handleDelete(index) {
+    const updatedData = data2.filter((element, i) => index !== i);
+    setData2(updatedData);
+  }
 
-
-    
-}
- 
   return (
     <>
       <h3>Data In Table</h3>
-      <table style={{border:"2px solid Indigo"}}>
+      <table style={{ border: "2px solid Indigo" }}>
         <thead>
-             <tr>
+          <tr>
             <th>Data Number</th>
-             <th>Data</th>
-             <th>Buttons</th>
-        </tr>
-
+            <th>Data</th>
+            <th>Buttons</th>
+          </tr>
         </thead>
         <tbody>
-             {data2.map((dtn,i)=>(<tr key={i+1}>
-            <td>{i+1}</td>
-            <td>{dtn.title}</td>
-            <td><button onClick={()=>handleDelete(i)}>delete</button></td>
-        </tr>))}
-
+          {data2.map((dtn, i) => (
+            <tr key={i + 1}>
+              <td>{i + 1}</td>
+              <td>{dtn.title}</td>
+              <td>
+                <button onClick={() => handleDelete(i)}>delete</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
-       
-       
       </table>
     </>
   );
