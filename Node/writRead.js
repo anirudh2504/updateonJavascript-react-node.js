12f1bea7da30dccbd8d8845a407de1dd0a7c35fe
Anirudh2504 <anirajput20022@gmail.com>
2025-08-29 15:06:15 +0530

    commit

--- Node/writRead.js
+++ Node/writRead.js
@@ -1,6 +1,27 @@
-import { readFileSync, writeFileSync } from 'fs';
-const readedData=readFileSync('./TextFiles/read.txt','utf-8')
-console.log(readedData);
-const writedata=`WE are reading data from read file and write thatv data in write file which have empty data ${readedData}`
-writeFileSync('./Textfiles/write.txt',writedata)
-console.log("Data written successfull")
+// const fs = require('fs');
+
+//----------------------Sync/Blocking way---------------------------------------------
+// const readedData = fs.readFileSync('./TextFiles/read.txt', 'utf-8');
+// console.log(readedData);
+
+// const writedata = `We are reading data from the read file and writing that data into the write file which had empty content: \n\n${readedData}`;
+// fs.writeFileSync('./TextFiles/write.txt', writedata);
+// console.log("Data written successfully");
+
+//------------------------------Async/Non-Blocking way---------------------------------
+// fs.readFile('./TextFiles/read.txt', 'utf-8', (err, data) => {
+//     if (err) {
+//         console.error("Error reading file:", err);
+//         return;
+//     }
+//     console.log("File content:", data);
+// });
+// console.log("Data Fetching...");
+
+//------------------------------Web server--------------------
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from Server");
+});
+server.listen(7000, "127.0.0.1", () => console.log("Listning on port 7000"));
