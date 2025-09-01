5b4288f073da5e14c5bd3b0b7f7d0d7b7e779ca0
Anirudh2504 <anirajput20022@gmail.com>
2025-09-01 14:52:33 +0530

    commit

--- Node/Eventloop/eventloop.js
+++ Node/Eventloop/eventloop.js
@@ -1,18 +1,33 @@
-const fs=require('fs')
+const fs = require("fs");
+setTimeout(() => {
+  console.log("Timer 1 executed"), 3000;
+});
 
-fs.readFile("test-file.txt",()=>{
-    console.log("I/O task finished")
+setImmediate(() => {
+  console.log("Timer of SetImmediate executed ");
+});
 
-    setTimeout(()=>{
-    console.log("Timer 1 executed")
-,0})
-setTimeout(()=>{
-    console.log("Timer 2 executed")
-,3000})
+fs.readFile("test-file.txt", () => {
+  console.log("I/O task finished");
+  console.log("------------------------------");
 
-setImmediate(()=>{
-    console.log("Timer of SetImmediate executed ")
-})
-})
+  setTimeout(() => {
+    console.log("Timer 2 executed"), 3000;
+  });
 
-console.log("Hello from Top level Code") 
+  setTimeout(() => {
+    console.log("Timer 1 executed"), 0;
+  });
+  setTimeout(()=>{
+    console.log("Set timeout with 3 sec time after without time")
+  },3000)
+
+  setImmediate(() => {
+    console.log("Timer of SetImmediate 2 executed ");
+  });
+  process.nextTick(() => {
+    console.log("Process next tick function executed");
+  });
+});
+
+console.log("Hello from Top level Code");
