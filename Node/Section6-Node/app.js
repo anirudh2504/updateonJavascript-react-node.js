9dad5e6b73a6c15350aa6104b5c9326827e991c8
Anirudh2504 <anirajput20022@gmail.com>
2025-09-02 16:51:08 +0530

    commit

--- Node/Section6-Node/app.js
+++ Node/Section6-Node/app.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 app.use(express.json()); //middleware  -----
 
+
+
+//-------------------------------Custom middleware-----------------------//
+// app.use((req,res,next)=>{
+//   console.log("Hello from middleWare")
+//   next()
+// })
+
 // app.get('/',(req,res)=>{
 //     res.status(200).json({message:"Hello from server Side",app:"Natours"})
 
@@ -14,7 +22,8 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-const getAllTours = (req, res) => {                        // ---------------------------->get All Tours
+const getAllTours = (req, res) => {
+  // ---------------------------->get All Tours
   res.status(200).send({
     status: 'success',
     results: tours.length,
@@ -23,7 +32,8 @@ const getAllTours = (req, res) => {                        // ------------------
     },
   });
 };
-const getSingleTour = (req, res) => {                        //--------------------->get single tour
+const getSingleTour = (req, res) => {
+  //--------------------->get single tour
   const id = req.params.id * 1; // convert string to number
   const tour = tours.find((el) => el.id === id);
 
@@ -44,7 +54,8 @@ const getSingleTour = (req, res) => {                        //-----------------
   });
 };
 
-const addNewTour = (req, res) => {                        //----------------------------->add new tours
+const addNewTour = (req, res) => {
+  //----------------------------->add new tours
   //   console.log(req.body);
   const newId = tours[tours.length - 1].id + 1; //Add new id to the data
   const newTour = Object.assign({ id: newId }, req.body); //object.assign assign values including previous values
@@ -63,7 +74,8 @@ const addNewTour = (req, res) => {                        //--------------------
     }
   );
 };
-const updateTour = (req, res) => {                   //------------------------------------Update Tour
+const updateTour = (req, res) => {
+  //------------------------------------Update Tour
   if (req.params.id * 1 > tours.length) {
     return res.status(404).send({
       status: '404',
@@ -80,7 +92,8 @@ const updateTour = (req, res) => {                   //-------------------------
   });
 };
 
-const deleteTour = (req, res) => {                  //---------------------------------delete tour
+const deleteTour = (req, res) => {
+  //---------------------------------delete tour
   if (req.params.id * 1 > tours.length) {
     return res.status(404).send({
       status: '404',
@@ -96,23 +109,50 @@ const deleteTour = (req, res) => {                  //--------------------------
 };
 
 
-
+const getAllUsers=(req,res)=>{
+  res.status(500).json({
+  status:'error',
+  message:"This route is Not Yet Defined"
+  })
+}
+const createUser=(req,res)=>{
+  res.status(500).json({
+  status:'error',
+  message:"This route is Not Yet Defined"
+  })
+}
+const getUser=(req,res)=>{
+  res.status(500).json({
+  status:'error',
+  message:"This route is Not Yet Defined"
+  })
+}
+const updateUser=(req,res)=>{
+  res.status(500).json({
+  status:'error',
+  message:"This route is Not Yet Defined"
+  })
+}
+const deleteUser=(req,res)=>{
+  res.status(500).json({
+  status:'error',
+  message:"This route is Not Yet Defined"
+  })
+}
 
 //-------------------------All Request------------------------------//
 app.get('/api/v1/tours', getAllTours);
-
 app.get('/api/v1/tours/:id', getSingleTour);
-
 app.post('/api/v1/tours', addNewTour);
-
 app.patch('/api/v1/tours/:id', updateTour);
-
 app.delete('/api/v1/tours/:id', deleteTour);
 
+app.get('/api/v1/users',getAllUsers)
+app.post('/api/v1/users',createUser)
 
-
-
-
+app.get('/api/v1/users/:id',getUser)
+app.patch('/api/v1/users/:id',updateUser)
+app.delete('/api/v1/users/:id',deleteUser)
 
 const port = 4000;
 app.listen(port, () => {
