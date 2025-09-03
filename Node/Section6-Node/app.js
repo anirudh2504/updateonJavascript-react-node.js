5dac9d21d29c370d75fdd419a5b55ef2ceb53e11
Anirudh2504 <anirajput20022@gmail.com>
2025-09-03 17:43:54 +0530

    commit all changes

--- Node/Section6-Node/app.js
+++ Node/Section6-Node/app.js
@@ -1,7 +1,21 @@
-
+const mongoose = require('mongoose');
 const express = require('express');
+const dotenv = require('dotenv');
 const app = express();
-app.use(express.json()); //middleware  -----
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+
+mongoose
+.connect(DB).then(con=>{
+  console.log(con.connections)
+  console.log("DB connection Successful")
+})
+// app.use(express.json()); //middleware  -----
 
 // const testMiddleware=(req,res,next)=>{
 //   console.log("I am a custom middleware")
@@ -9,15 +23,11 @@ app.use(express.json()); //middleware  -----
 // }
 // app.use(testMiddleware)
 
-
-
 //-------------------------------Custom middleware-----------------------//
-
 
 // const router=expreess.router();
 // const auth=(req,res,next)=>{
 //   console.log("I am inside auth middleware")
-
 
 //   req.user={
 //     id:1,
@@ -32,7 +42,6 @@ app.use(express.json()); //middleware  -----
 //       message:"Not a valid user"
 //     })
 //   }
-
 
 // }
 // const isStudent=(req,res,next)=>{
@@ -62,13 +71,12 @@ app.use(express.json()); //middleware  -----
 // }
 
 // router.get('/student',auth,isStudent,(req,res)=>{
-  
+
 //   res.send("Student Specific Page")
 // })
 // router.get('/admin',auth,isAdmin,(req,res)=>{
 //   res.send("Admin Page")
 // })
-
 
 // app.use((req,res,next)=>{
 //   console.log("Hello from middleWare")
@@ -83,20 +91,15 @@ app.use(express.json()); //middleware  -----
 //     res.send("You Can post To EndPoint")
 // })
 
-const tourRouter=require('./routes/tourRoutes')
-const userRouter=require('./routes/userRoutes')
+const tourRouter = require('./routes/tourRoutes');
+const userRouter = require('./routes/userRoutes');
 
-
-
-
-
-app.use('/api/v1/tours',tourRouter)
-app.use('/api/v1/users',userRouter)
+app.use('/api/v1/tours', tourRouter);
+app.use('/api/v1/users', userRouter);
 
 const port = 4000;
 app.listen(port, () => {
   console.log(`listening on Port ${port}`);
 });
-
 
 // module.exports=router
